b8520ecd4390ecce7620852d2e761ae69964cd10
Bridger Strickland <bridger210strick@gmail.com>
2022-01-13 11:49:48 -0700

    MVP

---
+++ Shopping list/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Shopping list
+//
+//  Created by Bridger Strickland on 1/13/22.
+//
+
+import Foundation
+
+class Item
+{
+    var name:String
+    
+    init(theName:String)
+    {
+        self.name = theName
+    }
+}
--- Shopping list/ViewController.swift
+++ Shopping list/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items : [Item] = []
+    
+            override func viewDidLoad()
+            {
+                tableView.dataSource = self
+                
+                let item1 = Item(theName:"Milk")
+                let item2 = Item(theName:"Eggs")
+                items = [item1, item2]
+            }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+            {
+        return items.count
+            }
+    func tableView(_ _tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+            UITableViewCell
+            {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row].name
+                return cell
+            }
+    @IBAction func itemButton(_ sender: Any) {
     }
-
-
+    //        super.viewDidLoad()
+        // Do any additional setup after loading the view.
 }
 
+
+
+
+   
